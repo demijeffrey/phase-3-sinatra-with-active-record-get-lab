c0d83e7a16a5faec918f34b9d843582a1f944c1f
Demaris Ashmore <demiashmore@yahoo.com>
2022-12-14 20:55:18 -0700

    commit tree 8f15572183e6f57629ff4170a69df2f647b3e920

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,33 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    all_goods = Bakery.all.map do |bakery|
+      bakery.baked_goods
+    end
+    sorted_goods = all_goods.order("price DESC")
+    sorted_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    all_goods = Bakery.all.map do |bakery|
+      bakery.baked_goods
+    end
+    all_goods.maximum(:price).to_json
+  end
 
 end
